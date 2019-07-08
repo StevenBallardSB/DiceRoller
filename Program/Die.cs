b40fb2f1ca1d011649b57fdc0f964a3bd7c70c99
steven ballard <stevenballardstudios@gmail.com>
2019-07-08 13:28:55 -0700

    Add project files.

---
+++ Program/Die.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    /// <summary>
+    /// Represents a single multi-sided die
+    /// </summary>
+    class Die
+    {
+
+    }
+}
