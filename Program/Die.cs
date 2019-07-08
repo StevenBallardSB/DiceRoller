4247d554d70a2e374f5f5b39301c66f2da303408
steven ballard <stevenballardstudios@gmail.com>
2019-07-08 14:44:34 -0700

    Added test Code, and finished class

--- Program/Die.cs
+++ Program/Die.cs
@@ -14,10 +14,52 @@ namespace Program
         private byte faceValue;
         private bool isHeld;
         private byte numberOfSides;
+        private static Random rand;
+
+        static Die()
+        {
+            //Share once instance of Random across All instance of the Die class
+            rand = new Random();
+        }
+
+        public Die() : this(6)
+        {
+        }
+
+        public Die(byte numberOfSides)
+        {
+            Roll();
+        }
+
+        public byte FaceValue
+        {
+            get { return faceValue;}
+            private set
+            {
+                if(value == 0)
+                {
+                    throw new Exception("die value cannot be 0");
+                }
+                
+            }
+        }
 
         public byte Roll()
         {
-            throw new NotImplementedException();
+            if (isHeld)
+            {
+                return FaceValue;
+            }
+
+            const byte MinValue = 1;
+            //offset because of exclusive max value in the random class
+            byte MaxValue = (byte)(numberOfSides + 1);
+
+            byte result = (byte)rand.Next(MinValue, MaxValue);
+
+            faceValue = result;
+
+            return result;
         }
     }
 }
